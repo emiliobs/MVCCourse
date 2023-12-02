60a8e6a69513affaf93fd937741b610d65dc0fa1
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-12-02 15:06:19 +0000

    add class categories

---
+++ WebApp/Program.cs
@@ -0,0 +1,21 @@
+
+using System.Net.Mime;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+       name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}");   
+
+
+app.Run();
+
+
+
